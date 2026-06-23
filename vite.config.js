9e73f163bf077fa7239a196e8b5c3bf2e46bc553
kelvimguidini <kelvimguimg@gmail.com>
2026-06-22 22:20:33 -0300

    554

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,29 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: 'localhost',
+export default defineConfig(({ command, mode }) => {
+    return {
+        base: mode === 'production' ? '/antigo/build/' : '/build/',
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            strictPort: true,
+            hmr: {
+                host: 'localhost',
+            },
         },
-    },
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-        manifest: true,
-    },
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
-            refresh: true,
-        }),
-        vue(),
-    ],
+        build: {
+            outDir: 'public/build',
+            emptyOutDir: true,
+            manifest: true,
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                ssr: 'resources/js/ssr.js',
+                refresh: true,
+            }),
+            vue(),
+        ],
+    }
 })
